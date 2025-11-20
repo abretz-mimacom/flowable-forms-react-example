0e9fd1d722ef021a2205fc57cf4c7a2a18666406
Aaron Bretz <aaron.bretz@mimacom.com>
2025-11-20 09:11:07 -0600

    add typescript

--- src/FlowableAppView.tsx
+++ src/FlowableAppView.tsx
@@ -1,10 +1,20 @@
 import './App.css';
 import { FlowableFlowApp } from '@flowable/work-views'
 import '@flowable/work-views/dist/index.css'
+import React from 'react';
+
+// Type declarations for window.flowable
+declare global {
+    interface Window {
+        flowable: {
+            httpClientCustomConfiguration?: (io: any) => void;
+        };
+    }
+}
 
 window.flowable.httpClientCustomConfiguration = function (io) {
     //add custom headers
-    io.interceptors.request.use(function (config) {
+    io.interceptors.request.use(function (config: any) {
         config.headers = {
             ...config.headers,
             "Authorization": "Basic " + btoa("admin:test")
@@ -13,11 +23,11 @@ window.flowable.httpClientCustomConfiguration = function (io) {
     })
 };
 
-function FlowableAppViewDemo() {
+const FlowableAppViewDemo: React.FC = () => {
     return (
         <div>
             <FlowableFlowApp
-                flowAppId="skeletonApp"
+                flowAppId="breadcrumbExample"
                 container="my-container"
                 showTopNavigationElements={true}
                 hideNavigationElements={true}
